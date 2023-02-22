73d843b034b261954b47630476db5f3b84d14580
QA-Aidan <aidanaedy.qa@gmail.com>
2023-02-22 12:50:35 +0000

    adding the storage package to contain test files

---
+++ Storage/First.java
@@ -0,0 +1,16 @@
+package Storage;
+
+public class First {
+    
+    public static void main(String[] args){
+        int age = 1500;
+        char ring = 'o';
+        long c = 60000;
+        float d = 0.01f;
+        String name = "Sauron";
+        
+        System.out.println(name.toUpperCase()+ " is = " + age + " years old, he is searching for the one ring " 
+        + ring + ", he has an army of " + c + " orcs & goblins and is " + d + "% popular (not very)!");
+    }
+}
+
